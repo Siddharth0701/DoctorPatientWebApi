23ff5151fb2351742c3b2fea23266a073bd7463e
Siddharth <siddharth.singh0701@gmail.com>
2022-07-19 15:09:51 +0530

    first commit

---
+++ DoctorPatientWebApi/Controllers/SkillsController.cs
@@ -0,0 +1,55 @@
+﻿using DoctorPatient.Data;
+using DoctorPatient.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DoctorPatientWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        SkillsRepository repository = null;
+        // skillsRepository=new SkillsRepository(context)
+        public SkillsController( DemoContext context)
+        {
+            this.repository = new SkillsRepository(context);
+        }
+        // GET: api/<SkillsController>
+        [HttpGet]
+        public IEnumerable<Skill> Get()
+        {
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SkillsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SkillsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SkillsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SkillsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
